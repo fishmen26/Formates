e700e8bfba1582fa90d6e7a27bb3b45ef4b4e01f
Daniel Yu <manfaiyu26@gmail.com>
2024-08-08 08:34:27 +0800

    Added Image and FlatList

--- app/(tabs)/community.tsx
+++ app/(tabs)/community.tsx
@@ -1,28 +1,34 @@
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, Image } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { ScrollView } from 'react-native';
+
+const AfTechLab = require("../../assets/images/AF_Tech_Lab.png");
+const SillcionClub = require("../../assets/images/silicon_valley.png");
+const FintechHub = require("../../assets/images/fintech_innovation_hub.png");
+
+
 
 const Community = () => {
 
   const COMMUNITY =[
     {
       id: 1,
-      imagePath: "",
+      imagePath: AfTechLab,
       communityName: "AF Tech Lab"
     },
     {
       id: 2,
-      imagePath: "",
+      imagePath: SillcionClub,
       communityName: "Silicon Valley Finance and Technology Club"
     },
     {
       id: 3,
-      imagePath: "",
+      imagePath: FintechHub,
       communityName: "Fintech Innovation Hub"
     }
   ]
-
 
   return (
     <SafeAreaView className="bg-white h-full">
@@ -30,13 +36,18 @@ const Community = () => {
         data={COMMUNITY}
         keyExtractor={item => item.id}
         renderItem={({item})=>(
-          <View className="px-4 py-2 justify-between flex-row">
-            <Text className="flex-col text-lg font-imedium">{item.communityName}</Text>
-            <Text className="flex-col"><MaterialIcons name="keyboard-arrow-right" size={24} color="black" /></Text>
+          <View className="flex flex-row px-4 py-3 justify-between align-middle">
+            <View className="flex-row">
+              <Image className="flex w-1/5" source={item.imagePath} 
+              style={{width: 40, height: 40}}/>
+              <View className="flex w-10"></View>
+              <Text className="flex w-70 text-lg font-imedium">{item.communityName}</Text>
+            </View>
+            <View><MaterialIcons name="keyboard-arrow-right" size={24} color="black" /></View>
           </View>
         )}
         ListHeaderComponent={()=>(
-          <View className="my-6 px-4 space-y-6">
+          <View className="my-2 px-4 space-y-6">
             <View>
               <View className="justify-between item-start flex-row mb-6">
                 <Text className="font-isemiBold text-xl">
@@ -47,10 +58,36 @@ const Community = () => {
             </View>
           </View>
         )}
-
-        
       />
-    </SafeAreaView>
+      <FlatList 
+        horizontal
+        data={COMMUNITY}
+        keyExtractor={item => item.id}
+        renderItem={({item})=>(
+          <View className="flex flex-row px-4 py-3 justify-between align-middle">
+            <View className="flex-row">
+              <Image className="flex w-1/5" source={item.imagePath} 
+              style={{width: 40, height: 40}}/>
+              <View className="flex w-10"></View>
+              <Text className="flex w-70 text-lg font-imedium">{item.communityName}</Text>
+            </View>
+            <View><MaterialIcons name="keyboard-arrow-right" size={24} color="black" /></View>
+          </View>
+        )}
+        ListHeaderComponent={()=>(
+          <View className="my-2 px-4 space-y-6">
+            <View>
+              <View className="justify-between item-start flex-row mb-6">
+                <Text className="font-isemiBold text-xl">
+                  My Community
+                </Text>
+                <Text className="text-primary font-isemiBold">See all {'>'}</Text>
+              </View>
+            </View>
+          </View>
+        )}
+      />
+      </SafeAreaView>
   )
 }
 
---
+++ assets/index.jsx
@@ -0,0 +1,9 @@
+import AfTechLab from './images/AF_Tech_Lab.png';
+import FintechClub from './images/fintech_innovation_hub.png';
+import SilliconClub from './images/silicon_valley.png';
+
+export default {
+    AfTechLab,
+    FintechClub,
+    SilliconClub,
+  };
